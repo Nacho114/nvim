eda8e30ec1951a346dbf481f770d9721946e7ab1
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-03-24 21:35:23 -0300

    added lualine

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,13 @@
 return {
     'lewis6991/gitsigns.nvim',
     opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+        -- See `:help gitsigns.txt`
+        signs = {
+            add = { text = '+' },
+            change = { text = '~' },
+            delete = { text = 'x' },
+            topdelete = { text = '‾' },
+            changedelete = { text = '~' },
+        },
     },
 }
